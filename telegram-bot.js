f840ff3ea41868fc7b6b811e0d5e1a7cd929c23e
Fernando Iguibejeres <fernando.iguibejeres@mercadolibre.com>
2016-12-24 20:02:10 -0300

    Add on message reaction

--- telegram-bot.js
+++ telegram-bot.js
@@ -8,38 +8,47 @@ let bot = new TelegramBot('311477110:AAEPYL1lz75Gh52NgJfVbhwYbNnR56rqtIM', { pol
 let errorHanler = (err) => { bot.sendMessage(msg.chat.id, "Me siento mal, te contesto cuando me recupere");}
 
 bot.onText(/^\/movie$/, function (msg) {
-    movieFetcher.getTitle(errorHanler, (titleResult) => {
-        console.log(`Responding /movie to ${msg.chat.id} with : ${titleResult}`); 
-        bot.sendMessage(msg.chat.id, titleResult);
-    });
+	movieFetcher.getTitle(errorHanler, (titleResult) => {
+		console.log(`Responding /movie to ${msg.chat.id} with : ${titleResult}`); 
+		bot.sendMessage(msg.chat.id, titleResult);
+	});
 });
 
 bot.onText(/^\/chota$/, function (msg) {
-    movieFetcher.getTitle(errorHanler, (titleResult) => {
-        console.log(`Responding /chota to ${msg.chat.id} with : ${titleResult}`);
-        bot.sendMessage(msg.chat.id, titleResult);
-    })
+	movieFetcher.getTitle(errorHanler, (titleResult) => {
+		console.log(`Responding /chota to ${msg.chat.id} with : ${titleResult}`);
+		bot.sendMessage(msg.chat.id, titleResult);
+	})
 });
 
 bot.onText(/^\/chota (.*)/, function (msg, match) {
-    let titleResult = utils.changeRandom(match[1])
-    console.log(`Responding /chota ${match[1]} to ${msg.chat.id} with : ${titleResult}`);
-    bot.sendMessage(msg.chat.id, titleResult);
+	let titleResult = utils.changeRandom(match[1])
+	console.log(`Responding /chota ${match[1]} to ${msg.chat.id} with : ${titleResult}`);
+	bot.sendMessage(msg.chat.id, titleResult);
 });
 
 bot.onText(/\/ask (.+)/, function (msg) {
-    console.log(`Responding 'Tu hermana!!! 'to ${msg.chat.id}`);
-    bot.sendMessage(msg.chat.id, "Tu hermana!!!");
+	console.log(`Responding 'Tu hermana!!! 'to ${msg.chat.id}`);
+	bot.sendMessage(msg.chat.id, "Tu hermana!!!");
 });
 
 bot.onText(/\/quien (.+)/, function (msg) {
-    console.log(`Responding 'Tu hermana!!! 'to ${msg.chat.id}`);
-    bot.sendMessage(msg.chat.id, "Tu hermana!!!");
+	console.log(`Responding 'Tu hermana!!! 'to ${msg.chat.id}`);
+	bot.sendMessage(msg.chat.id, "Tu hermana!!!");
 });
 
 bot.onText(/\/echo (.+)/, function (msg, match) {
-    console.log(`Echo ${match[1]} to ${msg.chat.id}`);
-    bot.sendMessage(msg.chat.id, match[1]);
+	console.log(`Echo ${match[1]} to ${msg.chat.id}`);
+	bot.sendMessage(msg.chat.id, match[1]);
+});
+
+bot.on('message', function (msg) {
+	if(/chota/.test(msg.text)) {
+		movieFetcher.getTitle(errorHanler, (titleResult) => {
+			console.log(`Responding onMessage to ${msg.chat.id} with : ${titleResult}`);
+			bot.sendMessage(msg.chat.id, titleResult);
+		});
+	}
 });
 
 console.log("Bot in running...");
