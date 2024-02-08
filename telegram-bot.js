4990c885bcdd36135a3be52b51284f8e1d31fc97
Fernando Iguibejeres <fernando.iguibejeres@mercadolibre.com>
2024-02-08 16:43:22 -0300

    update dependencies

--- telegram-bot.js
+++ telegram-bot.js
@@ -4,7 +4,7 @@ const TelegramBot = require('node-telegram-bot-api');
 const movieFetcher = require("./movie-fetcher");
 const utils = require('./utils');
 const bot = new TelegramBot(process.env.TELEGRAM_KEY, { polling: true });
-let errorHandler = (err) => { console.log(err) }
+let errorHandler = (err) => { console.error(err) }
 
 
 bot.onText(/^\/movie$/, function (msg) {
